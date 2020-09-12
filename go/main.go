becc07f33264126e383aaca758cbbb814eaafb5f
utgwkk <utagawakiki@gmail.com>
2020-09-12 09:45:14 +0000

    /api/.+/search に対応するtrace idをX-NewRelic-Trace-Idヘッダに入れる

--- go/main.go
+++ go/main.go
@@ -422,6 +422,9 @@ func postChair(c echo.Context) error {
 
 func searchChairs(c echo.Context) error {
 	ctx := c.Request().Context()
+	txn := newrelic.FromContext(ctx)
+	meta := txn.GetTraceMetadata()
+	c.Response().Header().Set("X-NewRelic-Trace-Id", meta.TraceID)
 	conditions := make([]string, 0)
 	params := make([]interface{}, 0)
 
@@ -724,6 +727,9 @@ func postEstate(c echo.Context) error {
 
 func searchEstates(c echo.Context) error {
 	ctx := c.Request().Context()
+	txn := newrelic.FromContext(ctx)
+	meta := txn.GetTraceMetadata()
+	c.Response().Header().Set("X-NewRelic-Trace-Id", meta.TraceID)
 	conditions := make([]string, 0)
 	params := make([]interface{}, 0)
 
